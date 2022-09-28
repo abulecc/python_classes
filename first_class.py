3ead7fa9f34e183aa5ea93c53a8d243e41f58f80
Emmanuel Aregbesola <105214836+abulecc@users.noreply.github.com>
2022-09-28 04:51:17 -0700

    (i love her)

---
+++ first_class.py
@@ -0,0 +1,56 @@
+#print("4")
+#print("5.4")
+#print(" This is a string")
+
+#print(type("4.5"))
+
+
+#variable assignment 
+
+# from __future__ import division
+# from ast import Num
+# import numbers
+# from re import A
+# from tokenize import Number
+
+
+# my_number = 400
+# my_num = 10
+
+# my_num
+# print (my_number/my_num)
+
+# + addition
+# - subtraction
+# / normal division
+# % modulus division
+# // floor division
+# * multiplication
+# ** exponential 
+
+# a = 4
+# b = 10
+
+# x =4*b - 2*a 
+
+# print(x)
+
+# average = (2+2+9+8+7+4)/6
+
+# print(average)
+
+# a = 4
+# b = 7
+# c = 2
+# numerator = (-b) + (4*a*c)**0.5
+# denominator = 2*a 
+
+# x = numerator/denominator
+
+# print(x)
+
+my_num = "09131779219"
+
+print(my_num)
+
+
