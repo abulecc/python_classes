20597c329b56f8aacbb1af49bc2f710192c7e5a2
Emmanuel Aregbesola <stillemmanueldevon@gmail.com>
2022-10-18 14:50:13 -0700

    StringConcatenation

--- StringConcatenation/class_4.py
+++ StringConcatenation/class_4.py
@@ -34,10 +34,126 @@
 
 # print(first_name[0:3] + " " + first_name[-3:])
 
-first_name = input("first name: ")
-last_name = input("last name: ")
+# first_name = input("first name: ")
+# last_name = input("last name: ")
 
-username = last_name[:3] + first_name[-3:]
-print(username)
+# username = last_name[:3] + first_name[-3:]
+# print(username)
+
+# my_name = "emmanuel"
+# print(my_name.upper())
+
+# my_name = "EMMANUEL"
+# print(my_name.lower())
+
+# my_name = "emmanuel"
+# print(my_name.capitalize())
+
+# my_name = "emmanuel"
+# print(my_name.isalpha())
+
+# my_name = "emmanuel"
+# print(my_name.isnumeric())
+
+# my_name = "I am a good boy"
+# my_name .index
+
+# a = "prosper"
+# if len(a) > 5:
+#     print("yes")   
+# elif len(a) == 5:
+#     print("no")
+# else:
+#     print("accurate")
+
+# a = input ("Enter your name")
+# if len(a) > 10:
+#     print("yes")
+# elif len(a)==7:
+#     print("accurate")
+# else:
+#     print("no")
+
+# username = input("enter your name: ")
+# if "a" in username:
+#     print("Invalid")
+# elif "c" in username:
+#     print("not allow")
+# elif "e" in username:
+#     print("error")    
+# else:
+#     print("username saved")
+
+# a = []
+# print(type(a))
+
+from audioop import avg
+from pickle import APPEND
+from re import L
+
+
+# a = [1, 2, 3]
+# print(a)
+
+# a = []
+# print(type(a))
+
+     
+# a = [1, 2, 3]
+# b = ["mango", "apple", "orange"]
+# print(a+b) 
+
+# a = [1,2,3]
+# b=[5,6]
+# c = [7,8]
+# d = [9,10]
+# print((a+b+c+d))
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+# print(a[2:8])
+
+# a = [111,222,333,444]
+# print(set(a))
+
+# a = "prosper"
+# print(list(a))
+
+# new_name = []
+# for letter in a:
+# new_name.append(letter)
+# print(new_name)
+
+# a = [2,3,4,5,8]
+# total=sum(a)
+# print(total)
+
+# a=[2,3,4,5,8]
+# print(min(a))
+
+# a=[2,3,4,5,8]
+# print(max(a))
+
+# a = [2,3,4,5,8]
+# last_number = a[-1]
+# first_number = a[0]
+# a.remove(first_number)
+# a.remove(last_number)
+# print(a)
+
+# a =[2,3,4,5,8]
+# len (a) = 5
+# a = 5//2
+# print(a[middle])
+
+# my_dict = {
+#     "a":"1",
+#     "b":"2",
+#     "c":"3"
+# }
+# print(my_dict["a"])
+
+a = [2,4,8,15,22,55]
+total = sum(a)
+print(total//6)
 
 
